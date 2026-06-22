301263afcf2ba5c3a0ace189d7e216f374c71e86
Jubaidi <djoebed@outlook.com>
2026-06-22 11:48:37 +0700

    Add files via upload

---
+++ Company Profile/script.js
@@ -0,0 +1,5 @@
+document.getElementById('kontakForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Terima kasih! Pesan Anda telah terkirim.');
+    // Anda bisa menambahkan kode untuk mengirim data formulir ke server di sini
+});
